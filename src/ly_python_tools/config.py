e9249fd0d18449e296a24779aabec055a95ffdb5
Liam Damewood <30477025+liam-ly@users.noreply.github.com>
2022-03-26 17:28:53 -0700

    Fix some mistakes

--- src/ly_python_tools/config.py
+++ src/ly_python_tools/config.py
@@ -1,3 +1,4 @@
+"""Helper functions for dealing with the pyproject file."""
 from __future__ import annotations
 
 from pathlib import Path
@@ -5,6 +6,7 @@ from typing import Sequence
 
 
 def get_pyproject(config_name: Path | str = "pyproject.toml") -> Path:
+    """Get the location of pyproject.toml in the first parent diretory."""
     cwd = Path.cwd().absolute()
     paths = [cwd] + list(cwd.parents)
     for path in paths:
@@ -18,5 +20,6 @@ class NoProjectFile(Exception):
     """No project file could be found."""
 
     def __init__(self, proj_filename: Path | str, search_paths: Sequence[Path]):
+        super().__init__()
         self.proj_filename = str(proj_filename)
         self.search_paths = [path.as_posix() for path in search_paths]
