2d1c4da1ac1ead314101c5f97f9bbd11f47e875d
Liam Damewood <30477025+liam-ly@users.noreply.github.com>
2022-03-29 20:52:05 -0700

    Fix issues with pyright by using venv in the project

--- tests/test_environ.py
+++ tests/test_environ.py
@@ -1,17 +1,7 @@
 """Test the environment helpers."""
 from __future__ import annotations
 
-import os
-from ly_python_tools.pyproper import environ
 
-
-def test_environ():
-    """Test that context manager environ works as expected."""
-    key = "__TEST_VAR__"
-    value = "__TEST_VAR_VALUE__"
-
-    old_env = os.environ.copy()
-    assert key not in os.environ
-    with environ(**{key: value}):
-        assert os.environ[key] == value
-    assert os.environ == old_env
+def test_noop():
+    """Empty test so pytest doesn't exit with non-zero."""
+    assert True
